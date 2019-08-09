f92ecdd7dd8d0cf64c43f9ffd2df4e2478c5023f
Andrew <cc6.magister@gmail.com>
2019-08-09 17:36:04 +0300

    update(editor): update placeholder

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1,33 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Konva from 'konva';
-import { Stage, Layer, Star, Text } from 'react-konva';
+import {Stage, Layer, Star, Image} from 'react-konva';
+import useImage from 'use-image';
+
+
+const CanvasImage = ({src}) => {
+	const [image] = useImage(src);
+	return <Image image={image} />;
+};
 
 class Editor extends Component {
+	state = {
+		width: 1200,
+		height: 768,
+	};
 
-	render(){
-		return(
-			<Stage width={window.innerWidth} height={window.innerHeight}>
+	render() {
+
+		return (
+			<Stage width={this.state.width} height={this.state.height} className='editor'>
 				<Layer>
-					<Text text="Try to drag a star" />
-					{[...Array(10)].map((_, i) => (
-						<Star
-							key={i}
-							x={Math.random() * window.innerWidth}
-							y={Math.random() * window.innerHeight}
-							numPoints={5}
-							innerRadius={20}
-							outerRadius={40}
-							fill="#89b717"
-							opacity={0.8}
-							draggable
-							rotation={Math.random() * 180}
-							shadowColor="black"
-							shadowBlur={10}
-							shadowOpacity={0.6}
-							onDragStart={this.handleDragStart}
-							onDragEnd={this.handleDragEnd}
-						/>
-					))}
+					<CanvasImage src={`https://via.placeholder.com/${this.state.width}x${this.state.height}/2d2d2d/fff`} />
 				</Layer>
 			</Stage>
 		)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
 import Editor from './components/editor';
 import './index.scss';
 
-function App() {
-	return (
-		<div className="App">
-			<Header/>
-			<Editor/>
-		</div>
-	);
+class App extends Component {
+
+	render() {
+		return (
+			<div className="App">
+				<Header/>
+				<Editor/>
+			</div>
+		);
+	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
